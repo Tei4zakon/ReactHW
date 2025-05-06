247e266c6db696210cad4006d2419bc89355c0eb
Tei4zakon <dimitrova.tea@gmail.com>
2025-05-06 19:30:51 +0200

    minor changes

---
+++ homework1/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './../css/Content.css';
+
+export function Footer() {
+    return (
+        <div>
+            <h4 className="footer-style">Thank you for reviewing my first homework</h4>
+        </div>
+    )
+};
